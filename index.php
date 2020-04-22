c582f5c51792ab568eb07c3a61b2e1a18bb6e392
blizstone <44795862+blizstone@users.noreply.github.com>
2020-04-22 13:42:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <html>
-<body>
-<h1> Hello World </h1>
+<head>
+		<title></title>
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+<body style="overflow-x:hidden;">
+		<section id="nav">
+			<a href="index.php" id="logo"></a>
+			<div id="nav-search-section">
+				<form action="search.php">
+					<input type="text" id="nav-search">
+					<input type="submit" id="nav-search-button" value="Search">
+				</form>
+			</div>
+			<div id="nav-top">
+				<p><a href="profile.php" class="nav-top-link">Profile</a></p>
+				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
+				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
+				<p><a href="login.php" class="nav-top-link">Sign In</a></p>
+				<p><a href="join.php" class="nav-top-link" id="join">Join</a></p>
+			</div>
+		</section>
 </body>
-//this edit
+
 
 </html>
