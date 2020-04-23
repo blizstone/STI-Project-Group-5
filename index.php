a330423c4bc53d6fd723cfb4b7d89b8fc567cd84
blizstone <44795862+blizstone@users.noreply.github.com>
2020-04-23 21:06:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>ITGig: Find IT Professionals</title>
+		<title>STI scam alert site</title>
 		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body style="overflow-x:hidden;">
@@ -21,59 +21,15 @@
 			</div>
 		</section>
 	
-		<section id="header">
-			<div id="headertext">GIG IT: Find an IT Professional</div>
-		</section>
-		
-		<section id="titletext">
-			<h1 id="title">Welcome to GIG IT</h1>
-			<p class="descriptions">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales turpis eget risus lacinia commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id ligula sit amet diam tincidunt varius ut vel libero. Cras ultricies fermentum ipsum eget dignissim. Maecenas ante purus, vehicula ac dui ut, egestas condimentum diam. Nam mi augue, pulvinar sed dui dictum, tempus vulputate purus. In ultricies, lorem ac rhoncus facilisis, urna libero suscipit lectus, nec porttitor augue ipsum porttitor nulla. Donec ornare dolor nec justo sollicitudin fringilla. Nam at tincidunt quam. Morbi posuere tristique pharetra. Aenean malesuada, metus a blandit ornare, urna ex auctor sapien, eu faucibus dui turpis id dolor. Praesent tincidunt sed odio et commodo. Mauris at tortor sollicitudin, cursus eros eu, condimentum turpis. Quisque placerat efficitur iaculis.</p>
-		</section>
-		
-		<section id="content">
-			<h2 class="titletext">Popular Professional Services</h2>
-			<div id="service-home">
-				<div class="service-home-box">
-					<p class="service-home-box-text">Pentester</p>
-				</div>
-				<div class="service-home-box" id="threathunter">
-					<p class="service-home-box-text">Threat Hunter</p>
-				</div>
-				<div class="service-home-box">
-					<p class="service-home-box-text">IT Auditor</p>
-				</div>
-				<div class="service-home-box">
-					<p class="service-home-box-text">Web Developer
-				</div>
-				<div class="service-home-box">
-					<p class="service-home-box-text">UXID Designer</p>
-				</div>
-			</div>
-		</section>
-		
-		<section id="newsletter">
-			<div id="newsletter-image"></div>
-			<div id="newsletter-content">
-				<h3 id="newsletter-title">Have a Curious Mind?</h3>
-				<p class="descriptions">Sign up to our newsletter to learn more about the changes in the IT industry!</p>
-				<form action="newsletter_process.php">
-					<input type="email" id="newsletter-input">
-					<input type="submit" id="newsletter-submit" value="Sign Up">
-				</form>
-			</div>
-		</section>
-		
-		<section id="footer">
-			<div id="footer-nav">
-				<h3 id="footer-nav-title">Helpful Links</h3>
-			</div>
-			<div id="footer-newsletter">
-				<form action="newsletter_process.php">
-					<input type="email" id="footer-newsletter-input">
-					<input type="submit" id="footer-newsletter-submit" value="Notify Me!">
-				</form>
-			</div>
-		</section>
-		
+<?php
+ 
+$a =1;
+$b =2;
+$c =$a+$b;
+
+echo $c
+
+
+?>
 	</body>
 </html>
