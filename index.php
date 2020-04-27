9a14f70fcd6322b0835d82feda86e67d231414f4
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-04-27 16:27:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,40 @@
 <html>
-	<head>
-		<title>STI scam alert site</title>
-		<link rel="stylesheet" href="stylesheet.css">
-	</head>
-	<body style="overflow-x:hidden;">
-		<section id="nav">
-			<a href="index.php" id="logo"></a>
-			<div id="nav-search-section">
-				<form action="search.php">
-					<input type="text" id="nav-search">
-					<input type="submit" id="nav-search-button" value="Search">
-				</form>
-			</div>
-			<div id="nav-top">
-				<p><a href="profile.php" class="nav-top-link">Profile</a></p>
-				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
-				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
-				<p><a href="login.php" class="nav-top-link">Sign In</a></p>
-				<p><a href="join.php" class="nav-top-link" id="join">Join</a></p>
-			</div>
-		</section>
-	
-<?php
- 
-$a =1;
-$b =2;
-$c =$a+$b;
 
-echo $c
+<head>
+	<title>STI scam alert site</title>
+	<link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body style="overflow-x:hidden;">
+	<section id="nav">
+		<a href="index.php" id="logo"></a>
+		<div id="nav-search-section">
+			<form action="search.php">
+				<input type="text" id="nav-search">
+				<input type="submit" id="nav-search-button" value="Search">
+			</form>
+		</div>
+		<div id="nav-top">
+			<p><a href="profile.php" class="nav-top-link">Profile</a></p>
+			<p><a href="blog.php" class="nav-top-link">Blog</a></p>
+			<p><a href="listing.php" class="nav-top-link">Browse</a></p>
+			<p><a href="login.php" class="nav-top-link">Sign In</a></p>
+			<p><a href="join.php" class="nav-top-link" id="join">Join</a></p>
 
 
-?>
-	</body>
+		</div>
+	</section>
+
+	<?php
+
+	$a = 1;
+	$b = 2;
+	$c = $a + $b;
+
+	echo $c
+
+
+	?>
+</body>
+
 </html>
