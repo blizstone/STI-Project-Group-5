9d73f8d65cd7f3ac3c390cf9af45dafea2fb4b5e
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-04-30 21:09:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,146 +1,41 @@
-
-<?php
-session_start();
-//Database Configuration File
-include('config.php');
-error_reporting(0);
-print($_SESSION["logged_in"]);
-if(isset($_SESSION["logged_in"])) {
-    print("Session is set");
-    header("location:home.php");
-    }
-
-else if(isset($_POST['login']))
-{
-    
-    //Genrating random number for salt
-    if(@$_SESSION['randnmbr']==""){
-        
-        $Alpha22=range("A","Z");
-        $Alpha12=range("A","Z");
-        $alpha22=range("a","z");
-        $alpha12=range("a","z");
-        $num22=range(1000,9999);
-        $num12=range(1000,9999);
-        $numU22=range(99999,10000);
-        $numU12=range(99999,10000);
-        $AlphaB22=array_rand($Alpha22);
-        $AlphaB12=array_rand($Alpha12);
-        $alphaS22=array_rand($alpha22);
-        $alphaS12=array_rand($alpha12);
-        $Num22=array_rand($num22);
-        $NumU22=array_rand($numU22);
-        $Num12=array_rand($num12);
-        $NumU12=array_rand($numU12);
-        $res22=$Alpha22[$AlphaB22].$num22[$Num22].$Alpha12[$AlphaB12].$numU22[$NumU22].$alpha22[$alphaS22].$num12[$Num12];
-        $text22=str_shuffle($res22);
-        $_SESSION['randnum']= $text22;
-    }
-    
-    
-    // Getting username/ email and password
-    $uname=$_POST['username'];
-    $password=hash('sha256',$_POST['password']);
-   
-    
-    // Hashing with Random Number
-    $saltedpasswrd=hash('sha256',$password.$_SESSION['randnum']);
-    // Fetch stored password  from database on the basis of username/email 
-    $sql ="SELECT UserName,UserEmail,LoginPassword FROM userdata WHERE (UserName=:usname || UserEmail=:usname)";
-    $query= $dbh -> prepare($sql);
-    $query-> bindParam(':usname', $uname,  PDO::PARAM_STR);
-    $query-> execute();
-    $results=$query->fetchAll(PDO::FETCH_OBJ);
-    if($query->rowCount() > 0)
-    {
-        foreach ($results as $result) {
-            $fetchpassword=$result->LoginPassword;
-            // $role=$result->Role;
-
-            // hashing for stored password
-            $storedpass= hash('sha256',$fetchpassword.$_SESSION['randnum']);
-        }
-        //You can configure your cost value according to your server configuration.By Default value is 10.
-        $options = [
-            'cost' => 12,
-        ];
-        // Hashing of the post password
-        $hash= password_hash($saltedpasswrd,PASSWORD_DEFAULT, $options);
-        // Verifying Post password againt stored password
-        if(password_verify($storedpass,$hash)){
-            // if($role=='admin'){
-            //     $_SESSION["logged_in"] = '2';
-            // }
-            
-            $_SESSION["username_login"]= $uname;
-            $_SESSION["logged_in"] = '1';
-            
-        }
-        
-        else {
-            echo "<script>alert('Wrong password');</script>";
-            
-        }
-        
-    }
-    
-    
-    else{
-        echo "<script>alert('Invalid Details');</script>";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>PDO | Login form</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width", initial-scale=1.0>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>DigiScam</title>
 </head>
 <body>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <header id="showcase">
+        <h1>Welcome to DigiScam</h1>
+        <p>Our website provides comprehensive information regarding the latest and trending scams that is plaguing the country</p>
+        <a href="login.php" class="button">Login To Account</a>
+    </header>
+    <section id="section-a">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.</p>
+    </section>
 
-    <div id="login-overlay" class="modal-dialog">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h4 class="modal-title" id="myModalLabel">Login Form</h4>
-          </div>
-          <div class="modal-body">
-              <div class="row">
-                  <div class="col-xs-6">
-                      <div class="well">
-                          <form id="loginForm" method="post">
-                              <div class="form-group">
-                                  <label for="username" class="control-label">Username / Email id</label>
-                                  <input type="text" class="form-control" id="username" name="username"  required="" title="Please enter you username or Email-id" placeholder="email or username" >
-                                  <span class="help-block"></span>
-                              </div>
-                              <div class="form-group">
-                                  <label for="password" class="control-label">Password</label>
-                                  <input type="password" class="form-control" id="password" name="password" placeholder="Password" value="" required="" title="Please enter your password">
-                                  <span class="help-block"></span>
-                              </div>
-                             
-                           
-                              <button type="submit" class="btn btn-success btn-block" name="login">Login</button>
-                          </form>
-                      </div>
-                  </div>
-                  
-                      <p><a href="signup.php" class="btn btn-info btn-block">Yes please, register now!</a></p>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-<script type="text/javascript">
+    <section id="section-b">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.</p>
+    </section>
 
-</script>
+    <section id="section-c">
+        <div class="box-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.
+        </div>
+
+        <div class="box-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.
+        </div>
+
+        <div class="box-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.
+        </div>
+    </section>
 </body>
 
 </html>
+
 </html>
