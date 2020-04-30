ead750bc34dfffd0814253142b99ba2153f0d2aa
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-04-30 21:09:06 +0800

    Create adminhome.php

---
+++ adminhome.php
@@ -0,0 +1,75 @@
+<?php
+// Check if session is not registered, redirect back to main page.
+// Put this code in first line of web page.
+session_start();
+if (($_SESSION['logged_in'] == '2')) {
+    echo "Welcome " ;   
+}
+else 
+{
+        header("location:mjo.php");
+}
+?>
+
+<html>
+	<head>
+		<title>STI scam alert site</title>
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body style="overflow-x:hidden;" onload="">
+		<section id="nav">
+			<a href="index.php" id="logo"></a>
+			<div id="nav-search-section">
+				<form action="search.php">
+					<input type="text" id="nav-search">
+					<input type="submit" id="nav-search-button" value="Search">
+				</form>
+			</div>
+			<div id="nav-top">
+				<p><a href="q3.php" class="nav-top-link">Profile</a></p>
+				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
+				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
+				<p><a href="logout.php" class="nav-top-link">Logout</a></p>
+				<p><a href="adminupdate.php" class="nav-top-link" id="Members">Members</a></p>
+
+			</div>
+		</section>
+
+
+<?php
+$con = new mysqli("localhost","root","","scamtest");
+$count = 0;
+$query = mysqli_query($con,"SELECT * FROM `reports`");
+foreach($query as $row){
+ $count++;
+
+echo "<form name='form' method='post' action='listing_details.php'>";
+echo "<table width='100%'>";
+echo "<tr>";
+echo "<td>";
+echo "<section id='grid-container'>";
+echo "<div class='card'>";
+$postId= $row['postId'];
+echo "<input type='hidden' name='service_array' value=".$postId .">";
+
+echo"<p>". $row['username'] ."</p>";
+echo"<p>". $row['content'] ."</p>";
+echo"<p>"."$". $row['category']."</p>";
+//echo"<p>".$row["date_created"] ."</p>";
+echo"<input type='submit' id='detailsButton' value='More Details'>";
+echo"</div>
+ </section>
+</td>
+</tr>
+</table>
+</form>";
+?>
+<?php
+if($count == 3) { // three items in a row
+        echo '</tr><tr>';
+        $count = 0;
+    }
+} ?>
+ 
+</body>
+</html>
