c78e95d633eca3ae159742d72b4fb6840e7ff62f
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-01 12:13:22 +0800

    commit on master

--- homepage.php
+++ homepage.php
@@ -14,7 +14,7 @@
     <a href="index.php" class="button">Login or Create an Account</a>
   </header>
   <section id="section-a">
-    <p>Scams are designed to trick people into giving away their money or personal information. While many people think scams target only certain groups of individuals, the truth is anyone can be a victim of a scam. We are all at risk. That's why it is important that everyone stays informed and vigilant against scams.</p>
+    <p></p>
   </section>
   <section id="section-b">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.</p>
