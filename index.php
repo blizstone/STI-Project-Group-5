7806c0217e176a2ae5c07514bdc316c288d6c56a
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-02 01:10:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width-device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie-edge">
-<link rel="stylesheet" href="css/style.css">
-<title>DigiScam</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width-device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>DigiScam</title>
 </head>
+
 <body>
-<header id="showcase">
+  <header id="showcase">
     <h1>Welcome To DigiScam</h1>
     <p>Our country is plagued with scams, and you have the right to know on the current and trending scams. Join us in fighting against scams</p>
     <a href="login.php" class="button">Login or Create an Account</a>
@@ -31,6 +33,7 @@
     </div>
   </section>
 </body>
+
 </html>
 
 </html>
