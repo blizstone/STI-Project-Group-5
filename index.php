ad8e0986208fdbef5828ae9dd3eca223aea407ca
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-02 11:36:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <p>Scams are designed to trick people into giving away their money or personal information. While many people think scams target only certain groups of individuals, the truth is anyone can be a victim of a scam. We are all at risk. That's why it is important that everyone stays informed and vigilant against scams.</p>
   </section>
   <section id="section-b">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae.</p>
+    <p>Victims were cheated of at least $6.8 million in about 1,700 reported cases of loan scams between January and November in 2019. This is already more than three times the reported amount cheated from victims for the whole of 2018. Scammers have been using increasingly sophisticated methods to convince their victims into handing them their personal information. No one is immune to the tricks of the scammers, who have upgraded themsleves. The police has advised people to look out for the tell tale signs of a scam.</p>
   </section>
   <section id="section-c">
     <div class="box-1">
