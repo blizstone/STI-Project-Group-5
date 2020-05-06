a4edd1b2da8f5a219cb4f7d3b07deb947523c72d
blizstone <44795862+blizstone@users.noreply.github.com>
2020-05-06 19:27:13 +0800

    Update home.php

--- home.php
+++ home.php
@@ -3,14 +3,10 @@
 // Put this code in first line of web page.
 session_start();
 
-
 if (($_SESSION['logged_in'] == '1')) {
    
 }  else if(($_SESSION['logged_in'] == '2')){
 	header("Location: adminhome.php");
-	
-	
-
 }
 else {
         header("location:index.php");
@@ -49,28 +45,30 @@ else {
 <div class="topnav" id="myTopnav">
   <a href="home.php" class="active">Home</a>
   <a href="#news">News</a>
-  <a href="#contact">Contact</a>
+  <a href="create_post.php">Create</a>
   <a href="logout.php">logout</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
 </div>
 </body>
+
 <?php
 $con = new mysqli("localhost","root","","digiscam");
 $count = 0;
-$query = mysqli_query($con,"SELECT * FROM `post`");
+$query = $con->prepare("SELECT post.postId, post.title, post.content, post.category, userdata.UserName FROM userdata INNER JOIN post ON post.accountId=userdata.accountId");
 foreach($query as $row){
  $count++;
+}
 
-echo "<form name='form' method='post' action='listing_details.php'>";
+echo "<form name='form' method='post' action='post_details.php'>";
 echo "<table width='100%'>";
 echo "<tr>";
 echo "<td>";
 echo "<section id='grid-container'>";
 echo "<div class='card'>";
 $postId= $row['postId'];
-echo "<input type='hidden' name='service_array' value=".$postId .">";
+echo "<input type='hidden' name='post_array' value=".$postId .">";
 
 //echo "<div class='row'>";
 //echo "<div class='span6'>";
@@ -84,10 +82,11 @@ echo "<a class='star'></a>";
 echo "</div>";
 echo "</div>";                       
 
-echo"<p>". $row['accountId'] ."</p>";
+echo"<p>". $row['UserName'] ."</p>";
+echo"<p>".$row['title'] ."</p>";
 echo"<p>". $row['content'] ."</p>";
 echo"<p>"."$". $row['category']."</p>";
-//echo"<p>".$row["date_created"] ."</p>";
+
 echo"<input type='submit' id='detailsButton' value='More Details'>";
 echo"</div>
  </section>
@@ -101,7 +100,7 @@ if($count == 3) { // three items in a row
         echo '</tr><tr>';
         $count = 0;
     }
-} ?>
+?>
  
 </body>
 </html>
