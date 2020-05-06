808a4247b32f725cb31103c83ab8ce79421b45bb
blizstone <44795862+blizstone@users.noreply.github.com>
2020-05-06 16:11:31 +0800

    Update home.php

--- home.php
+++ home.php
@@ -20,37 +20,42 @@ else {
 
 <html>
 	<head>
-		<title>STI scam alert site</title>
-		<link rel="stylesheet" href="stylesheet.css">
+	<title>STI scam alert site</title>
+	<link rel="stylesheet" href="stylesheet.css">
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
 
-    	<script src="dist/upvote/upvote.vanilla.js"></script>
+    <script src="dist/upvote/upvote.vanilla.js"></script>
+    <link rel="stylesheet" href="dist/upvote/upvote.css">
+    <style type="text/css"></style>
 
-    	<link rel="stylesheet" href="dist/upvote/upvote.css">
-    	<style type="text/css"></style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	</head>
-	<body style="overflow-x:hidden;" onload="">
-		<section id="nav">
-			<a href="index.php" id="logo"></a>
-			<div id="nav-search-section">
-				<form action="search.php">
-					<input type="text" id="nav-search">
-					<input type="submit" id="nav-search-button" value="Search">
-				</form>
-			</div>
-			<div id="nav-top">
-				<p><a href="viewprofile.php" class="nav-top-link">Profile</a></p>
-				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
-				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
-				<p><a href="logout.php" class="nav-top-link">Logout</a></p>
-				
-			</div>
-		</section>
+	<script>
+		/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+		function myFunction() {
+  		var x = document.getElementById("myTopnav");
+  		if (x.className === "topnav") {
+    	x.className += " responsive";
+  		} else {
+    	x.className = "topnav";
+  		}
+	}
+	</script>
+</head>
 
-
+<body>
+<div class="topnav" id="myTopnav">
+  <a href="home.php" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="logout.php">logout</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+</body>
 <?php
 $con = new mysqli("localhost","root","","digiscam");
 $count = 0;
@@ -67,9 +72,8 @@ echo "<div class='card'>";
 $postId= $row['postId'];
 echo "<input type='hidden' name='service_array' value=".$postId .">";
 
-echo "<div class='row'>";
-echo "<div class='span6'>";
-echo "<h3>Basic</h3>";
+//echo "<div class='row'>";
+//echo "<div class='span6'>";
 echo "<div class='row'>";
 echo "<div class='span1'>";
 echo "<div class='upvote'>";
