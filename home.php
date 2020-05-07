93655a113ca5d84118a0e2747d3603f7206c9672
blizstone <44795862+blizstone@users.noreply.github.com>
2020-05-07 22:06:33 +0800

    Update home.php

--- home.php
+++ home.php
@@ -16,16 +16,14 @@ else {
 <html>
 	<head>
 	<title>STI scam alert site</title>
-	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="stylesheet" href="stylesheet.css"> <!-- general stylesheet -->
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+  <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+  <script src="dist/upvote/upvote.vanilla.js"></script>
+  <link rel="stylesheet" href="dist/upvote/upvote.css">
 
-    <script src="dist/upvote/upvote.vanilla.js"></script>
-    <link rel="stylesheet" href="dist/upvote/upvote.css">
-    <style type="text/css"></style>
-
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
 
 	<script>
 		/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -44,8 +42,10 @@ else {
 <div class="topnav" id="myTopnav">
   <a href="home.php" class="active">Home</a>
   <a href="#news">News</a>
+  <a href="#news">News</a>
   <a href="create_post.php">Create</a>
-  <a href="logout.php">logout</a>
+  <a href="viewprofile.php">Account</a> 
+  <a href="logout.php">Logout</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
@@ -55,10 +55,9 @@ else {
 <?php
 $con = new mysqli("localhost","root","","digiscam");
 $count = 0;
-$query = $con->prepare("SELECT post.postId, post.title, post.content, post.category, userdata.UserName FROM userdata INNER JOIN post ON post.accountId=userdata.accountId");
+$query = mysqli_query($con,"SELECT post.postId, post.title, post.content, post.category, userdata.UserName FROM userdata INNER JOIN post ON post.accountId=userdata.accountId");
 foreach($query as $row){
  $count++;
-}
 
 echo "<form name='form' method='post' action='post_details.php'>";
 echo "<table width='100%'>";
@@ -67,7 +66,7 @@ echo "<td>";
 echo "<section id='grid-container'>";
 echo "<div class='card'>";
 $postId= $row['postId'];
-echo "<input type='hidden' name='post_array' value=".$postId .">";
+echo "<input type='hidden' name='post_array' value=". $postId .">";
 
 //echo "<div class='row'>";
 //echo "<div class='span6'>";
@@ -84,7 +83,7 @@ echo "</div>";
 echo"<p>". $row['UserName'] ."</p>";
 echo"<p>".$row['title'] ."</p>";
 echo"<p>". $row['content'] ."</p>";
-echo"<p>"."$". $row['category']."</p>";
+echo"<p>". $row['category']."</p>";
 
 echo"<input type='submit' id='detailsButton' value='More Details'>";
 echo"</div>
@@ -99,7 +98,7 @@ if($count == 3) { // three items in a row
         echo '</tr><tr>';
         $count = 0;
     }
-?>
+} ?>
  
 </body>
 </html>
