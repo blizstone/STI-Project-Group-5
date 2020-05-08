86b5e3bc4c7f45440ec2f35c8397a24652a98a60
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-08 13:50:52 +0800

    Update deletecode.php

--- deletecode.php
+++ deletecode.php
@@ -1,13 +1,18 @@
 <?php
+
+session_start();
+if (($_SESSION['logged_in'] == '1')) {
+}else {
+        header("location:index.php");
+}
+
 $con = mysqli_connect("localhost","root","","digiscam");
 if (!$con){
     die('Could not connect: ' . mysqli_connect_errno());
 }
 $actiontype = $_POST["actiontype"];
 $delete = $actiontype === "Delete";
-    $UserName = $_POST["UserName"];
-        $query= $con->prepare("Delete from userdata where UserName = ?");
-        $query->bind_param('s', $UserName ); //bind the parameters
+        $query= $con->prepare("Delete from userdata where accountId ='".$_SESSION["accountId"]."'");
             if($query->execute()) {//execute query
             header("Location: logout.php");
             }else{
