65e1ae53fb17a12b7db980ad32104ec3869e7af0
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-08 13:51:06 +0800

    Update updateuser.php

--- updateuser.php
+++ updateuser.php
@@ -10,7 +10,56 @@ else {
 
 ?>
 <html>
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<script>
+    function checkUsernameAvailability() {
+      $("#loaderIcon").show();
+      jQuery.ajax({
+        url: "check_availability.php",
+        data: 'UserName=' + $("#UserName").val(),
+        type: "POST",
+        success: function(data) {
+          $("#UserName-availability-status").html(data);
+          $("#loaderIcon").hide();
+        },
+        error: function() {}
+      });
+    }
+  </script>
+  <!--Javascript for check email availability-->
+  <script>
+    function checkEmailAvailability() {
+      $("#loaderIcon").show();
+      jQuery.ajax({
+        url: "check_availability.php",
+        data: 'email=' + $("#email").val(),
+        type: "POST",
+        success: function(data) {
+          $("#email-availability-status").html(data);
+          $("#loaderIcon").hide();
+        },
+        error: function() {
+          event.preventDefault();
+        }
+      });
+    }
+  </script>
 <body>
+<div class="topnav" id="myTopnav">
+  <a href="home.php" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#news">News</a>
+  <a href="create_post.php">Create</a>
+  <a href="viewprofile.php">Account</a> 
+  <a href="logout.php">Logout</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+  <link rel="stylesheet" href="dist/upvote/upvote.css">
+
+</div>
+  
 <div class="hero-text">
 	<br>
 	<h1>Update Personal Details</h1>  
@@ -19,19 +68,17 @@ else {
 <form action="updatecode.php" method ="POST" name="listall">
 <table>   
         <br>
-        <br>
-        <input type="radio" name="actiontype" value="Update" required>Update
-        <br>        
+              
 </table>
 <h1> Update</h1>
 <table>
-	  <tr><th>Account Id:</th><td>Please input Account ID</td><td><input type="text" name="accountId"  pattern="[0-9]"></td></tr>
      <tr><th>Full Name:</th><td>Fully can contain Letters Only</td><td><input type="text" name="FullName"  pattern="[a-zA-Z\s]+" required/></td></tr>
-     <tr><th>User Name:</th><td>Username can contain any letters or numbers, without spaces 6 to 12 chars</td><td><input type="text" name="UserName" pattern="^[a-zA-Z][a-zA-Z0-9-_.]{5,12}$" required/></td><tr>
+     <tr><th>User Name:</th><td>Username can contain any letters or numbers, without spaces 6 to 12 chars</td><td><input type="text" id="UserName" name="UserName" onBlur="checkUsernameAvailability()" pattern="^[a-zA-Z][a-zA-Z0-9-_.]{5,12}$" title="User must be alphanumeric without spaces 6 to 12 chars" class="input-xlarge" required>
+</td><tr>
      <tr><th>User Email:</th><td>Provide valide Email</td><td><input type="email" name="UserEmail"required/></td></tr>
      <tr><th>Mobile Number:</th><td>Please provide valid number</td><td><input type="text" name="UserMobileNumber" pattern="[0-8]{8}" maxlength="8" required/></td></tr>
 	 <tr><th>User Password:</th><td>Password must contain atleast 4 numbers</td><td><input type="password" name="LoginPassword" pattern="^\S{4,}$" required/></td></tr>
-	 <td><input type="submit" value="Update"/></td>
+	 <td><input action="updatecode.php" method ="POST"  type="submit" value="Update"/></td>
 	
 </table>
 
@@ -84,27 +131,8 @@ font-size: 20px;
     </head>
     <br>
     <body 
-    style="overflow-x:hidden;" onload="">
-		<section id="nav">
-            <a href="index.php" id="logo" ></a>
-			<div id="nav-search-section">
-				<form action="search.php">
-					<input type="text" id="nav-search">
-					<input type="submit" id="nav-search-button" value="Search">
-				</form>
-			</div>
-			<div id="nav-top">
-				<p><a href="viewprofile.php" class="nav-top-link">Profile</a></p>
-				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
-				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
-				<p><a href="logout.php" class="nav-top-link">Logout</a></p>
-				<p><a href="join.php" class="nav-top-link" id="join">Join</a></p>
-			</div>
-		</section>
-		
-
-
-        <style>
+    style="overflow-x:hidden;" onload="">       
+    <style>
 
 img {
 height: 250%;
