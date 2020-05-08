7e01edf4f97db792081865fa003ba26e28eca04e
blizstone <44795862+blizstone@users.noreply.github.com>
2020-05-08 09:50:16 +0800

    Update home.php

--- home.php
+++ home.php
@@ -20,7 +20,8 @@ else {
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
-
+  <script src="dist/upvote/upvote.vanilla.js"></script>
+  <link rel="stylesheet" href="dist/upvote/upvote.css">
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
 
