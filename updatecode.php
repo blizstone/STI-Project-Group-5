3d4e38590994071c3487ba8d8139f900a7a772ab
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-08 13:51:03 +0800

    Update updatecode.php

--- updatecode.php
+++ updatecode.php
@@ -6,40 +6,23 @@ if (($_SESSION['logged_in'] == '1')) {
 }else {
         header("location:mjo.php");
 }
-
-//Establish connection to database named mock
 $con = mysqli_connect("localhost","root","","digiscam");
 if (!$con){
     die('Could not connect: ' . mysqli_connect_errno());
 }
-$actiontype = $_POST["actiontype"];
-$update = $actiontype === "Update";
-$delete = $actiontype === "Delete";
-//This part is for assigning variables to the textfields from Q2 based on their �name�
-if($update || $delete ){
-    
-    $accountId = $_POST["accountId"];
     $FullName = $_POST["FullName"];
     $UserName = $_POST["UserName"];
     $UserEmail = $_POST["UserEmail"];
     $UserMobileNumber = $_POST["UserMobileNumber"];
     $LoginPassword = $_POST["LoginPassword"];
-
     $hasedpassword=hash('sha256',$LoginPassword);
-    //Creates the table and the table headers
-    echo "<tr><td>". $accountId ."</td><td>". $FullName ."</td><td>". $UserName . "</td><td>". $UserEmail. "</td><td>". $UserMobileNumber ."</td><td>". $LoginPassword ."</td><</tr>";
-    echo $delete  ." ". $update ;
-    //include "q4.php";
-    if($update) // Update functionality
-        $query= $con->prepare("Update userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=?, LoginPassword=? where accountId =?");
-        $query->bind_param('sssiss', $FullName, $UserName, $UserEmail, $UserMobileNumber, $hasedpassword, $accountId);
+    $query= $con->prepare("Update userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=?, LoginPassword=? where accountId ='".$_SESSION["accountId"]."'");
+    $query->bind_param('sssis', $FullName, $UserName, $UserEmail, $UserMobileNumber, $hasedpassword);
     if ($query->execute()) //execute query
         header("Location: logout.php");
-
     else{
         echo $query->error;
         header("Location: updateuser.php");
-    }
     }
     $con->close();
 ?>
