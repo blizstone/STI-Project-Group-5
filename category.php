e942cfb93bcd7ca8aa463c778b65a16de5420b24
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-13 23:03:43 +0800

    commit on master

--- category.php
+++ category.php
@@ -5,10 +5,12 @@
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie-edge">
   <link rel="stylesheet" href="css/category.css">
+  <link rel="stylesheet" href="css/cattop.css">
   <title>Categories</title>
 </head>
 
-<body>
+
+
 <header id="showcase">
 <a href="" class="button">Cyber Scam</a>
 <a href="" class="button">Wangiri Scam</a>
