8246da1e7619a823c7c21f55949d88d45550e977
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-17 10:49:55 +0800

    commit on master

--- home.php
+++ home.php
@@ -43,7 +43,7 @@ else {
 <div class="topnav" id="myTopnav">
   <a href="home.php" class="active">Home</a>
   <a href="#news">News</a>
-  <a href="http://localhost/Project/">Categories</a>
+  <a href="category.php">Categories</a>
   <a href="create_post.php">Create</a>
   <a href="viewprofile.php">Account</a>
   <a href="viewprofile.php">Account</a>  
