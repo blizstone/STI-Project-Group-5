61057f78d340fadbf40e193859cdf36a27ec2a29
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-17 16:01:50 +0800

    Update config.php

--- config.php
+++ config.php
@@ -14,3 +14,27 @@ catch (PDOException $e)
 exit("Error: " . $e->getMessage());
 }
 ?>
+
+<?php
+error_reporting(0);
+class db{
+	var $con;
+	function __construct(){
+		$this->$con=mysqli_connect("localhost","root","") or die(mysqli_error());
+		mysqli_select_db($this->$con,"digiscam") or die(mysqli_error());
+	}
+	public function getRecords(){
+		$query="SELECT * from ";
+		$result=mysqli_query($this->$con,$query);
+		return $result;
+	}
+	public function getRecordsWhere($price){
+		$query="SELECT * from  where _price < """;
+		$result=mysqli_query($this->$con,$query);
+		return $result;
+	}
+	public function closeCon(){
+		mysqli_close($this->$con);
+	}
+}
+?>
