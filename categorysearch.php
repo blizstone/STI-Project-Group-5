b4e2e3def58b1b68e049b25740999b8070946d4d
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-17 23:12:25 +0800

    Update categorysearch.php

--- categorysearch.php
+++ categorysearch.php
@@ -1,10 +1,10 @@
 <table   class="table table-striped table-responsive">
 	<tr>
-		<th>Product id</th>
-		<th>Name</th>
-		<th>Description</th>
-		<th>Price</th>
-		<th>Quantity</th>
+		<th>Post id</th>
+		<th>Account id</th>
+		<th>Title</th>
+		<th>Content</th>
+		<th>Category</th>
 	</tr>
 <?php
 require('config.php');
@@ -14,11 +14,11 @@ $result1=$db->getCategoryRecords($_POST['selected_category']);
 
 while($row1=mysqli_fetch_array($result1)){
 	echo "<tr>
-		<td>".$row1['id']."</td>
-		<td>".$row1['']."</td>
-		<td>".$row1['']."</td>
-		<td>".$row1['']."</td>
-		<td>".$row1['']."</td>
+        <td>".$row['postId']."</td>
+        <td>".$row['accountId']."</td>
+        <td>".$row['title']."</td>
+        <td>".$row['content']."</td>
+        <td>".$row['category']."</td>
 	</tr>";
 }
 $db->closeCon();
