15b32426e9c0eb426d8f33cb0a2f724aa8198fcb
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-17 23:39:55 +0800

    Update categorysearch.php

--- categorysearch.php
+++ categorysearch.php
@@ -1,3 +1,20 @@
+<?php
+require 'config.php';
+// Check if session is not registered, redirect back to main page.
+// Put this code in first line of web page.
+session_start();
+if (($_SESSION['logged_in'] == '1')) {
+   
+}  else if(($_SESSION['logged_in'] == '2')){
+	header("Location: adminhome.php");
+}
+else {
+        header("location:index.php");
+}
+
+?>
+
+
 <table   class="table table-striped table-responsive">
 	<tr>
 		<th>Post id</th>
