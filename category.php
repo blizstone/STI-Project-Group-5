c3e194ecd8f027410f98eac41fd5a4435fed774e
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-17 11:04:25 +0800

    Update category.php

--- category.php
+++ category.php
@@ -27,13 +27,13 @@
 <body>
 <div class="container">
 	<br><br>
-	<center><h1><strong>Search/Filter data in php using Ajax</strong></h1></center>
+	<center><h1><strong>Categories</strong></h1></center>
 	<br>
 	<div class="row">
 		
 			
 		<form method="post" class="form-horizontal">
-			<label for="" class="control-label col-sm-3 col-sm-offset-2" > Less then: </label>
+			<label for="" class="control-label col-sm-3 col-sm-offset-2" > Select Categories </label>
 			<div class="col-sm-2" >
 				<select name="" class="form-control" id="_dropdown">
 					<option>---Select---</option>
