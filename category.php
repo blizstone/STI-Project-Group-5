aa758a53cf3466694dc43caba1f6f0db796e73d9
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-18 17:27:19 +0800

    Update category.php

--- category.php
+++ category.php
@@ -9,72 +9,48 @@ else {
         header("location:mjo.php");
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#ajaxdata").load("allcategory.php");
-			$("").change(function(){
-				var selected=$(this).val();
-				$("#ajaxdata").load("categorysearch.php",{selected_category: selected});
-			});
-			$("#refresh").click(function(){
-				$("#ajaxdata").load("allcategory.php");
-			});
-		});
-	</script>
-
-</head>
-<body>
-<div class="container">
-	<br><br>
-	<center><h1><strong>Categories</strong></h1></center>
-	<br>
-	<div class="row">
-		
-			
-		<form method="post" class="form-horizontal">
-			<label for="category" class="control-label col-sm-3 col-sm-offset-2" > Select Categories: </label>
-			<div class="col-sm-2" >
-				<select name="category" class="form-control" id="_dropdown">
-					<option>---Select---</option>
-					<option val="Lottery Scam">Lottery Scam</option>
-					<option val="Impersonation Scam">Impersonation Scam</option>
-					<option val="Loan Scam">Loan Scam</option>
-					<option val="PayPal Scam">PayPal Scam</option>
-					<option val="Phishing Scam">Phishing Scam</option>
-					<option val="Job Scam">Job Scam</option>
-					<option val="Apple Scam">Apple Scam</option>
-					<option val="Apple Scam">Apple Scam</option>
-				</select>
-			</div>
-			<button type="button" name="refresh" id="refresh" class="btn btn-primary">Refresh</button>
-		</form>
-
-	</div>
-	<br><br>
-	<div id="ajaxdata">
-		
-	</div>
-</div>
-
 <?php
-$con = new mysqli("localhost","root","","digiscam");
-$count = 0;
-$sql = "SELECT * FROM category INNER JOIN category ON post.category = selected_category";
-$result = mysqli_query($con, $sql);
- 
+
+//Establish connection to database named mock
+$con = mysqli_connect("localhost","root","","digiscam");
+if (!$con){
+    die('Could not connect: ' . mysqli_connect_errno());
+}
+
+
+$query= $con->prepare("Select postId, accountId, title, content, category  from post");   
+$query->execute(); 
+    $query->store_result();
+    $query->bind_result($postId, $accountId, $title, $content, $category);    
+if ($query->execute())
+
+
+
+
+
+
+    //Displays the header
+
+    echo "<table>";
+
+    while ($query->fetch()) {
+		
+
+
+		if($category=='Apple Scam')
+		
+        //starts listing the row
+        echo "
+ <tr><th>Post id:</th><td>". $postId ."</td></tr>
+ <tr><th>Account id:</th><td>". $accountId ."</td></tr><tr>
+ <tr><th>Title:</th><td>". $title ."</td></tr>
+ <tr><th>Content:</th><td>". $content ."</td></tr>
+ <tr><th>Category:</th><td>". $category ."</td></tr>";
+	}
+	
+
+$con->close();
+
 
  
 ?>
