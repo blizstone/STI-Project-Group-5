87167c7af5e34e46c9c1db49e48181258c053a8c
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-19 15:30:22 +0800

    Update category.php

--- category.php
+++ category.php
@@ -1,60 +1,10 @@
 <!DOCTYPE html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#ajaxdata").load("allrecords.php");
-			$("#price_dropdown").change(function(){
-				var selected=$(this).val();
-				$("#ajaxdata").load("search.php",{selected_price: selected});
-			});
-			$("#refresh").click(function(){
-				$("#ajaxdata").load("allrecords.php");
-			});
-		});
-	</script>
-
+	
 </head>
 
 <body>
-<div class="container">
-	<br><br>
-	<center><h1><strong>Categories</strong></h1></center>
-	<br>
-	<div class="row">
-		
-			
-		<form method="post" class="form-horizontal">
-			<label for="price" class="control-label col-sm-3 col-sm-offset-2" >Select Category: </label>
-			<div class="col-sm-2" >
-				<select name="price" class="form-control" id="price_dropdown">
-					<option>---Select---</option>
-					<option val=""></option>
-					<option val=""></option>
-					<option val=""></option>
-					<option val=""></option>
-					<option val=""></option>
-					<option val=""></option>
-					<option val=""></option>
-				</select>
-			</div>
-			<button type="button" name="refresh" id="refresh" class="btn btn-primary">Refresh</button>
-		</form>
 
-	</div>
-	<br><br>
-	<div id="ajaxdata">
-		
-	</div>
-</div>
 
 <?php
 // Check if session is not registered, redirect back to main page.
