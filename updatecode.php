4ba3c229730dd074ffd5aba3f40cdef0de6ef20e
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-20 20:01:01 +0800

    Update updatecode.php

--- updatecode.php
+++ updatecode.php
@@ -33,8 +33,8 @@ if (!$con){
   
     
    
-    $mail->Subject = "Verify your email";
-    $mail->Body = "<h2>Thank u for sign up</h2>
+    $mail->Subject = "Update verification";
+    $mail->Body = "<h2></h2>
                   <a href='localhost/STI-Project-Group-5/activation.php?eid={$email}&token={$token}'>Click here to verify</a>
                   <p>this link valid for 20 min</p>
                   ";
