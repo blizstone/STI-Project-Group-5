12e6dee99cf100cb029855f13b4337e4dea07a05
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-23 16:12:27 +0800

    Update adminq3.php

--- adminq3.php
+++ adminq3.php
@@ -23,32 +23,11 @@ $delete = "Delete";
     $query->bind_param('s', $accountId ); //bind the parameters
 
     if ($query->execute()){  //execute query
-    echo " Done ";
+		header("Location: adminupdate.php");
     }else{
     echo $query->error;
     echo "Error executing query.";
     }
     $con->close();
     ?>
-    <html>
-	<head>
-		<title>STI scam alert site</title>
-		<link rel="stylesheet" href="stylesheet.css">
-	</head>
-	<body style="overflow-x:hidden;" onload="">
-		<section id="nav">
-			<a href="index.php" id="logo"></a>
-			<div id="nav-search-section">
-				<form action="search.php">
-					<input type="text" id="nav-search">
-					<input type="submit" id="nav-search-button" value="Search">
-				</form>
-			</div>
-			<div id="nav-top">
-				<p><a href="blog.php" class="nav-top-link">Blog</a></p>
-				<p><a href="listing.php" class="nav-top-link">Browse</a></p>
-				<p><a href="logout.php" class="nav-top-link">Logout</a></p>
-				<p><a href="adminupdate.php" class="nav-top-link" id="Members">Members</a></p>
-
-			</div>
-		</section>
+  
