5b5a97a26aa3c3f73c82e16f5fff58fca2fbedac
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-24 20:00:43 +0800

    Update search.php

--- search.php
+++ search.php
@@ -23,6 +23,6 @@ else {
 <?php
     if (isset($_POST['submit-search'])){
         $search = mysqli_escape_string($con, $_POST['search']);
-        $sql = "SELECT * post";
+        $sql = "SELECT * FROM post WHERE title LIKE '%$search%' OR content LIKE '%$search%' OR category LIKE '%$search%' ";
     }
 ?>
