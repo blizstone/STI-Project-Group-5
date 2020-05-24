526c0078721d3fdbe7ff776941ed2b1e0e342265
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-24 11:10:24 +0800

    Update searchnews.php

--- searchnews.php
+++ searchnews.php
@@ -22,3 +22,21 @@ else {
     <input type="text" name="search" placeholder="Search">
     <button type="submit" name="submit-search"></button>
 </form>
+
+<div class="article-container">
+    <?php
+        $sql = "SELECT * FROM post";
+        $result = mysqli_query($con, $sql);
+        $queryResults = mysqli_num_rows($result);
+
+        if ($queryResults > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div>
+                    <h3>".$row['postId']."</h3>
+                    <p>".$row['postId']."</p>
+                </div>";
+
+            }
+        }
+    ?>
+</div>
