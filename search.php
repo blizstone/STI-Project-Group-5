71e53149e250a1150c16b5e005aa2117fd15df8e
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-24 16:00:18 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,1 +1,17 @@
+<?php
+require 'config.php';
+// Check if session is not registered, redirect back to main page.
+// Put this code in first line of web page.
+session_start();
+if (($_SESSION['logged_in'] == '1')) {
+   
+}  else if(($_SESSION['logged_in'] == '2')){
+	header("Location: adminhome.php");
+}
+else {
+        header("location:index.php");
+}
+
+?>
+
 <h1>Search Page</h1>
