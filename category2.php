4eafc8ccc45989b93b3d9030411098b817e76441
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-25 19:32:02 +0800

    Update category2.php

--- category2.php
+++ category2.php
@@ -1,4 +1,18 @@
+<?php
+require 'config.php';
+// Check if session is not registered, redirect back to main page.
+// Put this code in first line of web page.
+session_start();
+if (($_SESSION['logged_in'] == '1')) {
+   
+}  else if(($_SESSION['logged_in'] == '2')){
+	header("Location: adminhome.php");
+}
+else {
+        header("location:index.php");
+}
 
+?>
 
 <?php
 include 'config.php';
