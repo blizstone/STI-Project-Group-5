e6a78da2ebfae6e5f55454ab6265f794a95d9dae
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-25 18:27:23 +0800

    commit on master

--- search.php
+++ search.php
@@ -1,4 +1,17 @@
 <?php
+// Check if session is not registered, redirect back to main page.
+// Put this code in first line of web page.
+session_start();
+if (($_SESSION['logged_in'] == '2')) {
+}
+else 
+{
+        header("location:mjo.php");
+}
+?>
+
+
+<?php
     include 'header.php';
 ?>
 
