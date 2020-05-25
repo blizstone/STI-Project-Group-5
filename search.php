2af4baa4265d140dd32930c50a15359f5ede53d5
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-25 18:02:39 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,5 +1,15 @@
+<?php
+    include 'header.php';
+?>
+
+
 <h1>Search Page</h1>
 
 <div class="article-container">
-    
+    <?php
+        if (isset($_POST['submit-search'])) {
+            $search = mysqli_real_escape_string($con, $_POST['search']);
+            $sql = "SELECT * FROM post WHERE ";
+        }
+    ?>
 </div>
