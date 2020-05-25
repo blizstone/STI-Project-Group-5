55b96c08f2280c47f4af68799908e23d10dc2f0b
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-25 14:30:30 +0800

    Update searchnews.php

---
+++ searchnews.php
@@ -0,0 +1,8 @@
+<?php
+    include 'header.php';
+?>
+
+<form action="search.php" method="POST">
+    <input type="text" name="search" placeholder="Search">
+    <button type="submit" name="submit-search"></button>
+</form>
