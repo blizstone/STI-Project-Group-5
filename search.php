f8fe3dd9f49ab6273ec11b96b16b728b21cdaed8
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-25 18:12:09 +0800

    Update search.php

--- search.php
+++ search.php
@@ -9,7 +9,11 @@
     <?php
         if (isset($_POST['submit-search'])) {
             $search = mysqli_real_escape_string($con, $_POST['search']);
-            $sql = "SELECT * FROM post WHERE ";
+            $sql = "SELECT * FROM post WHERE postId LIKE '%$search%' OR accountId LIKE '%$search%' 
+                OR title LIKE '%$search%' OR content LIKE '%$search%' OR category LIKE '%$search%'";
+            $result = mysqli_query($con, $sql);
+            $queryResult = mysqli_num_rows($result);
+
         }
     ?>
 </div>
