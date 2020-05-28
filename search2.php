35a6c0c63b034b8225d2ae3545747ec5293f4be9
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-28 17:08:33 +0800

    Update search2.php

--- search2.php
+++ search2.php
@@ -6,5 +6,15 @@
 <h1>Search page</h1>
 
 <div class="article-container">
+    <?php
+        if (isset($_POST['submit-search'])) {
+            $search = mysqli_real_escape_string($conn, $_POST['search']);
+            $sql = "SELECT * FROM article WHERE postId LIKE '%$search%' OR accountId LIKE '%$search%' OR title LIKE '%$search%' OR content LIKE '%$search%' OR category LIKE '%$search%'";
+        }
+
+
+
+    ?>
+
 
 </div>
