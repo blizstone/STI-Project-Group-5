4a7739fe1ce013303a89fb0f2d695a4e5ecc329b
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-05-28 17:25:49 +0800

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -1,15 +1,12 @@
 <?php
-// Check if session is not registered, redirect back to main page.
-// Put this code in first line of web page.
 session_start();
 if (($_SESSION['logged_in'] == '2')) {
 }
 else 
 {
-        header("location:mjo.php");
+        header("location:index.php");
 }
 ?>
-
 <html>
 	<head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,10 +16,10 @@ else
   <link rel="stylesheet" href="./css/main.css">
   <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- navbar/voting stylesheet -->
   <link rel="stylesheet" href="css/login1.css">
-    <link rel="stylesheet" href="css/login2.css">
+  <link rel="stylesheet" href="css/login2.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
 
-    <link rel="stylesheet" href="dist/upvotejs/upvotejs.css">
+  <link rel="stylesheet" href="dist/upvotejs/upvotejs.css">
 	<script>//navbar script
 		/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 		function myFunction() {
@@ -35,23 +32,16 @@ else
 	}
 	</script>
 </head>
-
 <body>
 <div class="topnav" id="myTopnav">
-  <a href="adminhome.php" class="active">Home</a>
-  <a href="#news">News</a>
-  
-  <a href="adminupdate.php">Members</a>  
+  <a href="adminhome.php" class="active">Home</a>   
+  <a href="adminupdate.php">Members</a> 
+  <a href="adminpost.php">Posts</a>   
   <div class="pull-right"><a href="logout.php">Logout</a></div>
-
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
-  </a>
-
-  
+  </a>  
 </div>
-
-</body>
 <?php
 $con = new mysqli("localhost","root","","digiscam");
  $sql = "SELECT COUNT(*) totalCountByEachCategory, category FROM `post` WHERE category IS NOT NULL GROUP BY category ORDER BY totalCountByEachCategory DESC LIMIT 1";
@@ -61,45 +51,30 @@ $user_count = mysqli_query($con, $user);
 $row = mysqli_fetch_assoc($result);
 $post = "SELECT * FROM post";
 $col = mysqli_query($con, $post);
-
-
 ?>
-<!-- <div class="container mt-4">
-  <table>
-    <thead>
-      <tr>
-        <th class="table-danger" style="padding: 50px 400px 50px 50px">Category</th>
-        <th class="table-info" style="padding: 50px">Number Of Users</th>
-        <th class="table-warning" style="padding: 50px">Number Of Posts</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td class="table-danger" style="padding: 50px 400px 50px 50px"><?=$row['category']?></td>
-        <td class="table-info" style="padding: 50px"><?=$user_count->num_rows?></td>
-        <td class="table-warning" style="padding: 50px"><?=$col->num_rows?></td>
-      </tr>
-    </tbody>
-  </table>
-</div> -->
+
 
 <div class="container mt-5">
       <div class="row">
         <div class="col-md-12">
           <div class="widget-small primary"><i class="icon fa fa-users fa-3x" style="padding:60px"></i>
-            <div class="info">
-              <h4>Users</h4>
+            <div class="info">             
+              <h4>Users</h4>              
               <p><b><?=$user_count->num_rows?></b></p>
+              <form action="adminupdate.php" method="post">
+              <button class="more">More Details</button>
             </div>
+          </form>
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
           <div class="widget-small info"><i class="icon fa fa-list fa-3x" style="padding:60px"></i>
-            <div class="info">
-              <h4>Category</h4>
-              <p><b><?=$row['category']?></b></p>
+            <div class="info"><h4>Posts</h4>
+              <p><b><?=$col->num_rows?></b></p>
+              <form action="adminpost.php" method="post">
+              <button class="more">More Details</button>
             </div>
           </div>
         </div>
@@ -108,14 +83,23 @@ $col = mysqli_query($con, $post);
         <div class="col-md-12">
           <div class="widget-small warning"><i class="icon fa fa-paper-plane fa-3x" style="padding:60px"></i>
             <div class="info">
-              <h4>Posts</h4>
-              <p><b><?=$col->num_rows?></b></p>
+            <h4>Category</h4>
+              
+              <p><b><?=!empty($row['category'])?$row['category']: "No Post";?></b></p>
             </div>
           </div>
         </div>
       </div>
     </div>
+    </body>
 <style>
+  
+body {
+background-image: url("https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+background-repeat: no-repeat;
+background-size: 100%;
+}
+      
 thead,tbody {
   float: left;   
 }
@@ -144,6 +128,13 @@ tr:nth-child(even) {
 }
 .widget-small{
   margin-bottom: 1px ;
+}
+.more {
+color:black;
+font-size: 18px;
+font-family: Arial, Helvetica, sans-serif;
+background-color: lightblue;
+
 }
 </style>
  
