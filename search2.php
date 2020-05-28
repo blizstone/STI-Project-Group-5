2c8ee230485b94bc20c5a44ced25dc4149edee6d
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-28 17:24:31 +0800

    Update search2.php

--- search2.php
+++ search2.php
@@ -9,7 +9,27 @@
     <?php
         if (isset($_POST['submit-search'])) {
             $search = mysqli_real_escape_string($conn, $_POST['search']);
-            $sql = "SELECT * FROM article WHERE postId LIKE '%$search%' OR accountId LIKE '%$search%' OR title LIKE '%$search%' OR content LIKE '%$search%' OR category LIKE '%$search%'";
+            $sql = "SELECT * FROM article WHERE postId LIKE '%$search%' OR accountId LIKE '%$search%' OR 
+            title LIKE '%$search%' OR content LIKE '%$search%' OR category LIKE '%$search%'";
+            $result = mysqli_query($conn, $sql);
+
+            $queryResult = mysqli_num_rows($result);
+
+            if ($queryResult > 0){
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<div class='article-box'>
+                    <p>".$row['postId']."</p>
+                    <p>".$row['accountId']."</p>
+                    <p>".$row['title']."</p>
+                    <p>".$row['content']."</p>
+                    <h3>".$row['category']."</h3>
+                </div>";
+
+                }
+ 
+            } else {
+                echo "There are no results matching your search!";
+            }
         }
 
 
