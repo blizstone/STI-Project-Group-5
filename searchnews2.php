fbde71126b52b462797dedc4947a1d9703579cc4
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-28 11:27:49 +0800

    Update searchnews2.php

--- searchnews2.php
+++ searchnews2.php
@@ -32,12 +32,21 @@ else {
                 }
             }
 
+            function inactive(){
+                var searchBar = document.getElementById('searchBar');
+
+                if(searchBar.value == ''){
+                    searchBar.value == 'Search...'
+                    searchBar.placeholder = ''
+                }
+            }
+
         </script>
     </head>
 
     <body>
         <form action="search.php" method="$_POST">
-            <input type="text" id="searchBar" placeholder="" value="Search..." maxlength="25" autocomplete="on" onmousedown="" onblur="active();" /><input type="submit" id="searchBtn" value="Go!" />
+            <input type="text" id="searchBar" placeholder="" value="Search..." maxlength="25" autocomplete="on" onmousedown="active();" onblur="inactive();" /><input type="submit" id="searchBtn" value="Go!" />
 
 
         </form>
