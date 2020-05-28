444324de448ce5f180e8151f2a776ea8630bbf28
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-28 23:18:00 +0800

    commit on master

--- category2.php
+++ category2.php
@@ -26,6 +26,13 @@ $categoryResult = $db_handle->runQuery("SELECT DISTINCT category FROM post ORDER
 <link href="css/category.css" type="text/css" rel="stylesheet" />
 <title>Categories</title>
 </head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+	<link rel="stylesheet" href="stylesheet.css"> <!-- general/navbar stylesheet -->
+  
+
+  
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
 
 <body>
 <body>
