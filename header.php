756d975985f91b73a1eb9e0c5dad34577fa85b50
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-05-28 21:11:24 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+	<link rel="stylesheet" href="stylesheet.css"> <!-- general/navbar stylesheet -->
+  
+
+  
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
+
+  
+    
+    <title>Search Page</title>
     <link rel="stylesheet" type="text/css" href="css/search.css">
+
+    
+    <div class="topnav" id="myTopnav">
+        <a href="home.php" class="active">Home</a>
+        <a href="category2.php">Categories</a>
+        <a href="searchnews2.php">Search</a>
+        <a href="create_post.php">Create</a>
+        <a href="viewprofile.php">Account</a> 
+        <div class="pull-right"><a href="logout.php">Logout</a></div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+        </a>
+
+  
+    </div>
 </head>
 </html>
