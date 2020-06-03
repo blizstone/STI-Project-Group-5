68e0ea0b1ffb02302b922210ea7942cad66ba97e
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-06-03 14:19:23 +0800

    Update adminq3.php

--- adminq3.php
+++ adminq3.php
@@ -1,27 +1,25 @@
 <?php
-// Check if session is not registered, redirect back to main page.
-// Put this code in first line of web page.
 session_start();
 if (($_SESSION['logged_in'] == '2')) {
 }
 else {
         header("location:index.php");
 }
-
-//Establish connection to database named mock
 $con = mysqli_connect("localhost","root","","digiscam");
 if (!$con){
     die('Could not connect: ' . mysqli_connect_errno());
 }
-
 $delete = "Delete";
 
-    
     $accountId = $_POST["accountId"];
     
     $query1= $con->prepare("Delete from voting where accountId = $accountId");
     $query2= $con->prepare("Delete from post where accountId = $accountId");
     $query3= $con->prepare("Delete from userdata where accountId = $accountId");
+    $query1->bind_param('i', $accountId );
+    $query2->bind_param('i', $accountId );
+    $query3->bind_param('i', $accountId );
+    
     if($query1->execute()) {
       echo "Done";
       if($query2->execute()) {
@@ -33,6 +31,6 @@ $delete = "Delete";
         }
     }
   }
-    $con->close();
-    ?>
+  $con->close();
+?>
   
