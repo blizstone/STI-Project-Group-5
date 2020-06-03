92ad11051bcf62b3a4cf851f551ab3a8bccc22e2
elliwer1108 <64055160+elliwer1108@users.noreply.github.com>
2020-06-03 22:06:10 +0800

    here owen

--- comparison.php
+++ comparison.php
@@ -11,12 +11,27 @@ else {
 
 $con = new mysqli("localhost","root","","digiscam");
 
+$postId="";
+$count==0;
 
 
-$query = $con->prepare(SELECT `postId`, `content` FROM `post`);
+$query = $con->prepare("SELECT `postId`, `content` FROM `post` WHERE postId=?");
+$query-> bind_param("i",$count);
+$query-> execute();
+$query-> bind_result($postId,$content1);
+$query-> fetch();
+return $exists;
 
 
-
+function voteexist($con, $postId, $accountId){
+    $voteexist=$con->prepare("SELECT COUNT(*) FROM voting WHERE accountId =? AND postId=? ");
+    $voteexist->bind_param("ii", $accountId, $postId);
+    $voteexist->execute();
+    $voteexist->bind_result($exists);
+    $voteexist->fetch();
+ //   $con->close();
+    return $exists;
+}
 
 
 
