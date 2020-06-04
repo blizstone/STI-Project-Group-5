f25ef66775821298caec2e017ff746a2e0ef3c2e
DeadlyLoneWolf <58290494+DeadlyLoneWolf@users.noreply.github.com>
2020-06-04 20:38:39 +0800

    Update home.php

--- home.php
+++ home.php
@@ -58,6 +58,7 @@ else {
 </div>
 
 </body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>
     $( document ).ready(function() {
         input_value = $.trim($('#searchkey').val());
