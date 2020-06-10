a7845412e261db842f18e9365c2b75cec81432d0
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-06-10 16:05:07 +0800

    Update updatecode.php

--- updatecode.php
+++ updatecode.php
@@ -17,37 +17,53 @@ if (!$con){
     $UserName = $_POST["UserName"];
     $UserEmail = $_POST["UserEmail"];
     $UserMobileNumber = $_POST["UserMobileNumber"];
-    $LoginPassword = $_POST["LoginPassword"];
-    $confirm =$_POST['confirmpassword'];
-    $hasedpassword=hash('sha256',$LoginPassword);
+   
 
-    $email = $_POST['UserEmail'];
+
+
+    $query= $con->prepare("Select  UserName, UserEmail  from userdata");    
+    $result=$query->execute(); 
+    $query->store_result();
+    $query->bind_result($checkusername, $checkemail);
+
+    while($query->fetch())
+
+  
+    if ($UserName == $checkusername) {
+            echo "Username exist";
+        }
+        elseif ($UserEmail == $checkemail) {
+            echo "Email exist";
+        }
+        
+    else {
+
+
     
 
     $query= $con->prepare("Select  UserEmail  from userdata where accountId ='".$_SESSION["accountId"]."'");    
     $result=$query->execute(); 
     $query->store_result();
     $query->bind_result($Emailfromdb);
+
     while($query->fetch())
-    if($confirm != $LoginPassword){
-        
-        echo "Password and confirm password dosen't match";
-        header("Refresh: 1; url='updateuser.php");
-    } else {
+
+ 
     if ($Emailfromdb == $UserEmail) {
-        $query= $con->prepare("Update  userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=?, LoginPassword=? where accountId ='".$_SESSION["accountId"]."'");
-        $query->bind_param('sssis', $FullName, $UserName, $UserEmail, $UserMobileNumber, $hasedpassword);
+        $query= $con->prepare("Update  userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=? where accountId ='".$_SESSION["accountId"]."'");
+        $query->bind_param('sssi', $FullName, $UserName, $UserEmail, $UserMobileNumber);
         if ($query->execute()) {
             header("Location: viewprofile.php");
             } else {
                 echo "error";
             }
     } else {
-    $query= $con->prepare("Update  userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=?, LoginPassword=?, validation_key='$token', is_active=0 where accountId ='".$_SESSION["accountId"]."'");
-    $query->bind_param('sssis', $FullName, $UserName, $UserEmail, $UserMobileNumber, $hasedpassword);
+    $token = getToken(32);
+    $query= $con->prepare("Update  userdata SET FullName=?, UserName=?, UserEmail=?, UserMobileNumber=?, validation_key='$token', is_active=0 where accountId ='".$_SESSION["accountId"]."'");
+    $query->bind_param('sssi', $FullName, $UserName, $UserEmail, $UserMobileNumber);
     $mail->addAddress($_POST['UserEmail']);
 
-    $token = getToken(32);
+    
     $encode_token = base64_encode(urlencode($token));
     $emailencoded = base64_encode(urlencode($_POST['UserEmail']));
 
@@ -68,7 +84,7 @@ if (!$con){
  
         
     }
-}
+    }
 
     
   ?>
