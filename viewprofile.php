03e605b99173094b9ed30bd57170ca6a6cd07cfb
armondkjoy <41097067+armondkjoy@users.noreply.github.com>
2020-06-10 19:25:03 +0800

    Update viewprofile.php

--- viewprofile.php
+++ viewprofile.php
@@ -52,7 +52,7 @@ if (!$con){
   
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- navbar stylesheet -->
 
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/login1.css">
     <link rel="stylesheet" href="css/login2.css">
 	<script>//navbar script
