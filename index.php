ab15f4f13335fd43de56b07137802265ff3a41b0
elliwer1108 <64055160+elliwer1108@users.noreply.github.com>
2020-06-12 01:26:59 +0800

    haha

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie-edge">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" type="image/x-icon" href="favi.ico" />
   <title>DigiScam</title>
 </head>
 
